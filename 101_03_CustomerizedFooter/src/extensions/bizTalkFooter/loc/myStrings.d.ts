f72b93efa90e1975f863c82aee24e66dbb2733e9
zhong xu <xz@ms.local>
2019-12-05 09:33:19 +0800

    101_03 Footer extension demo is ok

---
+++ 101_03_CustomerizedFooter/src/extensions/bizTalkFooter/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IBizTalkFooterApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'BizTalkFooterApplicationCustomizerStrings' {
+  const strings: IBizTalkFooterApplicationCustomizerStrings;
+  export = strings;
+}
