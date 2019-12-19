a0dd78d4b7776aca57c621c20bfd128f738a2628
Zhong.Xu <xu.zhong@hotmail.com>
2019-12-19 16:58:17 +0800

    101_07 Cust Style Completed

--- 101_07_CustStyle/src/webparts/csutomerizedStyle/components/CsutomerizedStyle.tsx
+++ 101_07_CustStyle/src/webparts/csutomerizedStyle/components/CsutomerizedStyle.tsx
@@ -3,8 +3,18 @@ import { ICsutomerizedStyleProps } from './ICsutomerizedStyleProps';
 
 export default class CsutomerizedStyle extends React.Component < ICsutomerizedStyleProps, {} > {
   public render(): React.ReactElement<ICsutomerizedStyleProps> {
-    return(
-      <div>CSS was installed. ({this.props.CssFilePath})</div >
-    );
+    if(this.props.CssFilePath){
+      return (
+        <div></div>
+      );
+    }
+    else {
+      return(
+        <div>
+          <h2>需要先配置默认的CSS文件路径</h2>
+          <h3>最好将CSS放在站点资源库中</h3>
+        </div >
+      );
+    }
   }
 }
